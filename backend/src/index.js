d04f440a0176728401ecad82db02d711aef84a21
Himanshu <himanshuydv7777@gmail.com>
2024-04-16 00:36:15 +0530

    Implemented signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import connectDB from './db/index.js'
+import cors from 'cors'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -9,6 +10,7 @@ import authRouter from './routes/auth.routes.js'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/v1/auth', authRouter)
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <div>
-      this is app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
