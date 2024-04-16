cab05c04e28aa1f65b35c355c5959cc0434637bb
Himanshu <himanshuydv7777@gmail.com>
2024-04-16 11:43:19 +0530

    Implemented signin page

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+function SignIn() {
+    const [formData, setFormData] = useState(
+        {
+            username: '',
+            email: '',
+            password: ''
+        }
+    )
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setFormData(
+            {...formData, [e.target.id]: e.target.value}
+        )
+    }
+
+    const handleSignIn = async (e) => {
+        e.preventDefault()
+
+        fetch('/api/v1/auth/signin', {
+            method: "POST",
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(formData) 
+        })
+        .then((resp) => {
+            return resp.json()
+        })
+        .then((data) => {
+            if(data.success == false){
+                console.log(data.message)
+                return
+            }
+
+            setFormData(
+                {
+                    username: '',
+                    email: '',
+                    password: ''
+                }
+            )
+
+            console.log(data)
+            navigate('/')
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    return (
+        <div className='flex bg-gradient-to-br from-gray-50 to-amber-200 h-[100vh] w-[100vw] p-10'>
+            <div className='flex justify-center sm:justify-start w-full'>
+                <div className='hidden sm:flex justify-center w-[60%]'>
+                    <img className='h-[40vw]' src="/images/image-signin.png"/>
+                </div>
+
+                <div className='p-5'>
+                    <h1 className='text-3xl text-[#845EC2] font-semibold text-center'>
+                        Welcome to 
+                        <span className='font-bold font-serif text-[#402768]'> SwiftStore</span>
+                    </h1>
+
+                    <form onSubmit={handleSignIn} className='flex flex-col shadow-lg mt-7 border sm:border-dashed border-blue-800 px-5 py-7 rounded-xl'>
+                        <input value={formData.username} onChange={handleChange} className='p-3 rounded-xl focus:outline-none text-violet-900' type="text" id="username" placeholder='Username' />
+                        <span className="text-gray-500 text-center">or</span>
+                        <input value={formData.email} onChange={handleChange} className='p-3 rounded-xl focus:outline-none text-violet-900' type="email" id="email" placeholder='Email' />
+                        <input value={formData.password} onChange={handleChange} className='p-3 mt-9 rounded-xl focus:outline-none text-blue-600' type="password" id="password" placeholder='Password' />
+                        
+                        <button type='submit' className="relative mt-4 uppercase py-2 px-8 text-[#402768] text-base font-bold nded-full overflow-hidden bg-white rounded-xl transition-all duration-400 ease-in-out shadow-sm hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0" >
+                            sign in
+                        </button>
+
+                        <p className='text-gray-500 text-center mt-5 sm:mt-0'>
+                            Dont have an account?
+                            <Link to={'/signup'} className='text-blue-700 font-semibold opacity-75 hover:opacity-100'> Sign up</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
