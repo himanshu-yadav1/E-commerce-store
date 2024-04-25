b07fd838b16e810580748604f4475534240b6064
Himanshu <himanshuydv7777@gmail.com>
2024-04-25 20:16:07 +0530

    Added hero section to the homepage

---
+++ frontend/src/components/Hero_Section.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+function Hero_Section() {
+    return (
+        <>
+            <div className='flex flex-col sm:flex-row p-5 sm:p-0 pb-0 rounded-md bg-gradient-to-br from-[#e2f9ed] to-[#1D7B65]'>
+                <div className="flex flex-col sm:w-[55%] sm:p-5 md:p-20">
+                    <div>
+                        <h2 className="text-2xl sm:text-3xl sm:mt-10 uppercase text-center sm:text-start md:text-center font-semibold sm:text-[#1e4e40]">Discover Endless Shopping Possibilities</h2>
+                        
+                        <p className="font-mono md:pl-11 mt-8 sm:mt-14 text-center sm:text-start">Explore a world of endless options with us. From fashion essentials to the latest gadgets, find everything you need right here.</p>
+                    </div>
+                    
+                    <div className="hidden sm:flex justify-center py-5">
+                        <Link to={'/'} className=" bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 hover:scale-105 cursor-pointer transition">
+                            Shop Now
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="flex mt-5 md:mt-0">
+                    <img width='500' src="images/male-checking-his-phone.png" alt=""/>
+                </div>
+            </div> 
+        </>
+    )
+}
+
+export default Hero_Section
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Menu from '../components/Menu'
 import Category from '../components/Category'
 import { useSelector } from 'react-redux'
+import Hero_Section from '../components/Hero_Section'
+import { CiSearch } from 'react-icons/ci'
 
 function Home() {
 
@@ -10,9 +12,28 @@ function Home() {
     return (
         <div>
             <Menu/>
+
             
             <div className={`sm:hidden fixed flex justify-end h-[100vh] w-[100vw] bg-black bg-opacity-40 duration-500 transition-transform ${isCategoryPanelVisible ? '-translate-x-0' : '-translate-x-[-100%]'}`}>
                 <Category />
+            </div>
+
+
+            <div className='sm:hidden pt-7'>
+                <h1 className="text-center text-3xl font-extrabold text-[#1d3f35]">SwiftStore</h1>
+                
+                <form className="flex justify-between gap-2 mx-5 mt-5 p-2 border border-gray-300 rounded-lg">
+                    <input type="text" placeholder='Search for products...' className='w-full text-[#222E50] focus:outline-none'/>
+                    
+                    <button>
+                        <CiSearch className='w-10 text-[#222E50] size-6'/>
+                    </button>
+                </form>
+            </div>
+
+
+            <div className='p-5 sm:pt-0'>
+                <Hero_Section />
             </div>
 
         </div>
