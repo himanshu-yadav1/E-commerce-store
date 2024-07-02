df5bfc3d8402b5f491380b80750aff56089064c8
kartikgoyal89 <kartikgoyal794@gmail.com>
2024-07-02 22:45:06 +0530

    Order API Completed

--- backend/src/controller/order.controller.js
+++ backend/src/controller/order.controller.js
@@ -1,36 +1,91 @@
 import { isValidObjectId } from "mongoose";
 import errorHandler from "../utils/ErrorHandler";
 import { Order } from "../models/order.model";
+import { User } from "../models/user.model";
+import { Product } from "../models/product.model";
 
 const cancelOrder = async (req, res, next) => {
-    try {
-        const orderId = req.params.id;
+  try {
+    const orderId = req.params.id;
 
-        if (!isValidObjectId(orderId)) {
-            return next(errorHandler(400, "Order Id is not valid"));
-        }
-
-        const order = await Order.findById(orderId);
-
-        if (!order) {
-            return next(errorHandler(404, "Order not found"));
-        }
-
-        if (order.status === 'CANCELLED') {
-            return next(errorHandler(400, "Order already cancelled"));
-        }
-
-        // Check if the user is authorized to cancel the order
-
-        await Order.findByIdAndUpdate(orderId, { status: 'CANCELLED' });
-
-        res
-        .status(200)
-        .json({ message: "Order cancelled" });
-
-    } catch (error) {
-        next(error);
+    if (!isValidObjectId(orderId)) {
+      return next(errorHandler(400, "Order Id is not valid"));
     }
-}
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return next(errorHandler(404, "Order not found"));
+    }
+
+    if (order.status === "CANCELLED") {
+      return next(errorHandler(400, "Order already cancelled"));
+    }
+
+    // Check if the user is authorized to cancel the order
+
+    await Order.findByIdAndUpdate(orderId, { status: "CANCELLED" });
+
+    res.status(200).json({ message: "Order cancelled" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createOrder = async (req, res, next) => {
+  const { buyerId, address, orderItems, orderPrice, paymentMethod } = req.body;
+  try {
+    const buyer = await User.findById(buyerId);
+    if (!buyer) {
+      return next(errorHandler(404, "Invalid Buyer ID"));
+    }
+    for (let item of orderItems) {
+      const prdt = await Product.findById(item?.productId);
+      if (!product) {
+        return next(errorHandler(404, `Invalid Buyer ID ${item?.productId}`));
+      }
+    }
+    const newOrder = new Order({
+      buyerId,
+      address,
+      orderItems,
+      orderPrice,
+      paymentMethod,
+    });
+
+    const savedOrder = await Order.save();
+    res.status(201).json({
+      message: "Order Created Succesfully!",
+      order: newOrder,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getAllOrders = async (req, res, next) => {
+  try {
+    const getAllOrders = await Order.find({});
+    res.status(200).json(getAllOrders);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const getOrderDetails = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const findOrder = await Order.findById(id);
+    if (!findOrder) {
+      return next(errorHandler(404, "Order Not Found!"));
+    }
+    return res.status(200).json({ findOrder });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 export default cancelOrder;
--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import cancelOrder from "../controller/order.controller.js";
+import createOrder from "../controller/order.controller.js";
+import getAllOrders from "../controller/order.controller";
+import getOrderDetails from "../controller/order.controller";
 
-const router = Router()
+const router = Router();
 
-// router.post('/create', verifyJWT, )
-router.put('/cancel/:id', verifyJWT, cancelOrder)
-// router.post('/getAllOrders/:id', verifyJWT, )
-// router.post('/getOrderDetails/:id', verifyJWT, )
+router.post("/create", createOrder);
+router.put("/cancel/:id", verifyJWT, cancelOrder);
+router.post("/getAllOrders/:id", verifyJWT, getAllOrders);
+router.post("/getOrderDetails/:id", verifyJWT, getOrderDetails);
 
-export default router
+export default router;
