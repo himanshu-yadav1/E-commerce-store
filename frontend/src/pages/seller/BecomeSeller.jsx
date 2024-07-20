0c5a0ab02ee43474b1d78cbfcfb31eb5ce194c4f
Himanshu <himanshuydv7777@gmail.com>
2024-07-20 21:29:43 +0530

    Updated Become Seller page

--- frontend/src/pages/seller/BecomeSeller.jsx
+++ frontend/src/pages/seller/BecomeSeller.jsx
@@ -6,9 +6,10 @@ import { useDispatch, useSelector } from "react-redux"
 import { updateUser } from "../../features/userSlice"
 
 function BecomeSeller() {
-
     const [storeName, setStoreName] = useState('')
     const { currentUser } = useSelector((state) => state.user)
+
+    const [error, setError] = useState(null)
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -18,53 +19,73 @@ function BecomeSeller() {
 
         fetch('/api/v1/user/registerAsSeller', {
             method: 'POST',
-            headers: { 'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify(
-                { 
+                {
                     storeName,
                     userId: currentUser.user._id
                 }
             )
         })
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((data) => {
-            if(data.success == false){
-                console.log(data)
-                return
-            }
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((data) => {
+                if (data.success == false) {
+                    setError(data.message)
+                    return
+                }
 
-            dispatch(updateUser(data))
-            navigate('/seller/dashboard')       
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
+                dispatch(updateUser(data))
+                navigate('/seller/dashboard')
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
 
     return (
         <div >
             <Header />
+
             <HeaderBar />
 
-            <div className="p-6">
-                <p className="text-center text-lg text-red-400">You are not registerd as seller</p>
+
+            <div className="py-mainPaddingY px-mainPaddingX min-h-screen flex items-start justify-center">
+                <div className="bg-cardBg py-cardPaddingY px-cardPaddingX shadow-cardShadow border-cardBorderTop rounded-cardBorderRadius p-6 w-full max-w-md mt-4 sm:mt-1">
+                    <h2 className="text-center text-2xl font-semibold">Register Your Store</h2>
+
+                    <p className="text-center text-sm text-red-400">You are not registerd as seller</p>
+
+                    <form onSubmit={handleBecomeSeller}>
+                        <div className="mt-4">
+                            <label className="block text-gray-700 mb-1">Store Name</label>
+                            <input
+                                type="text"
+                                name="storeName"
+                                value={storeName}
+                                onChange={(e) => setStoreName(e.target.value)}
+                                className="w-full px-[13px] py-[6px] border border-gray-300 rounded-md focus:outline-none"
+                                required
+                            />
+                        </div>
+
+                        <p className="text-center text-red-500 mt-7">{error}</p>
+
+                        <div className="flex justify-center mt-2">
+                            <button
+                                type="submit"
+                                className="uppercase cursor-pointer duration-75 transition-all text-white px-[13px] py-[6px] rounded-md border border-[#ed957f] bg-[#ed957f] hover:bg-[#ec7558] active:brightness-90 focus:outline-none"
+                            >
+                                Register Store
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
-            <div className="flex gap-10 sm:gap-24 flex-col sm:flex-row items-center justify-center mx-5 p-6 pb-14 rounded-xl shadow-xl bg-gradient-to-br from-[#e2f9ed] to-[#1D7B65]">
-                <img className='w-[40vw] sm:w-[25vw]' src="../images/seller/seller.png" alt="" />
-
-                <form onSubmit={handleBecomeSeller} className="flex flex-col items-center gap-5">
-                    {/* ------------------error message------------ */}
-                    <input value={storeName} onChange={(e) => setStoreName(e.target.value)} className="text-lg p-2 rounded-lg border sm:w-80 focus:outline-none" type="text" placeholder="Enter your store name"/>
-                    <button type="submit" className="border border-white text-white font-mono font-semibold text-lg px-4 py-2 sm:py-1 rounded-lg shadow-md shadow-white duration-150 hover:shadow-white hover:shadow-lg active:scale-95 active:shadow-white active:shadow-sm">Register</button>
-                </form>
-            </div>
-
-            </div>
+        </div>
     )
 }
 
